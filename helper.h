355102f9b2b70c0509166519b68ccd5ba6d83de4
Gabe <gabeshin.ts@gmail.com>
2022-10-23 19:45:52 +0900

    leetcode included

---
+++ helper.h
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+class RandomNumberBetween
+{
+public:
+  RandomNumberBetween(int low, int high)
+      : random_engine_{std::random_device{}()}, distribution_{low, high}
+  {
+  }
+  int operator()()
+  {
+    return distribution_(random_engine_);
+  }
+
+private:
+  std::mt19937 random_engine_;
+  std::uniform_int_distribution<int> distribution_;
+};
